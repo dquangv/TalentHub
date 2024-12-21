332fae8b704cf54f8592efe1347d4f19bf4351dc
thanhtung795 <vothanhtung795@gmail.com>
2024-12-21 11:12:07 +0700

    code base backend

---
+++ backend/src/main/java/org/example/backend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Phục vụ tất cả các file trong thư mục ./uploads/images/
+        registry.addResourceHandler("/uploads/images/**")
+                .addResourceLocations("file:./uploads/images/");
+    }
+}
