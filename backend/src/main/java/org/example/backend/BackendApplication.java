7de031400eefc60ba1188b16cf5c0331442d81b7
dquangvu <dquangvu7998@gmail.com>
2024-12-24 15:34:08 +0700

    fix "variable not initialized in the default constructor" error

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package org.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = "org.example.backend")
+@SpringBootApplication
+@EnableConfigurationProperties()
+@ConfigurationPropertiesScan
 public class BackendApplication {
 
     public static void main(String[] args) {
