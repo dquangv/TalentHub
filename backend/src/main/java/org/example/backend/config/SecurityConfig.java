17e437b2db0d64e6422f3632a0d7172d44ee01ea
thanhtung795 <vothanhtung795@gmail.com>
2024-12-28 12:59:00 +0700

    fix refreshtoken

--- backend/src/main/java/org/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
             "/api/v1/auth/login",
             "/api/v1/auth/register",
-            "/api/v1/auth/refresh",
+            "/api/v1/auth/refresh-token",
             "/api/v1/auth/introspect",
             "/api/v1/auth/logout",
     };
