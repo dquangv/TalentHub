53cd5eb53528a20cd819d2e8bfcd63966e4ac2ba
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-11 13:29:13 +0700

    Update SchoolDTOResponse.java

--- backend/src/main/java/org/example/backend/dto/response/account/freelancer/SchoolDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/account/freelancer/SchoolDTOResponse.java
@@ -1,4 +1,12 @@
 package org.example.backend.dto.response.account.freelancer;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
 public class SchoolDTOResponse {
+
+    private Long id;
+    private String schoolName;
 }
