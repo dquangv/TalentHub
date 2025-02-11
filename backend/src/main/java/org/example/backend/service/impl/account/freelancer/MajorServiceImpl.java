9805251345bfb5b2b95897192cdfab32a2652729
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-11 13:42:25 +0700

    Update MajorServiceImpl.java

--- backend/src/main/java/org/example/backend/service/impl/account/freelancer/MajorServiceImpl.java
+++ backend/src/main/java/org/example/backend/service/impl/account/freelancer/MajorServiceImpl.java
@@ -3,34 +3,51 @@ package org.example.backend.service.impl.account.freelancer;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.request.account.freelancer.MajorDTORequest;
 import org.example.backend.dto.response.account.freelancer.MajorDTOResponse;
+import org.example.backend.entity.child.account.freelancer.Major;
+import org.example.backend.repository.MajorRepository;
 import org.example.backend.service.intf.account.freelancer.MajorService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MajorServiceImpl implements MajorService {
 
+    private final MajorRepository majorRepository;
+
     @Override
-    public MajorDTOResponse create(MajorDTORequest majorDTORequst) {
-        return null;
+    public MajorDTOResponse create(MajorDTORequest majorDTORequest) {
+        Major major = new Major();
+        major.setMajorName(majorDTORequest.getMajorName());
+
+        major = majorRepository.save(major);
+        return new MajorDTOResponse(major.getId(), major.getMajorName());
     }
 
     @Override
-    public Optional<MajorDTOResponse> getById(Long aLong) {
-        return Optional.empty();
+    public Optional<MajorDTOResponse> getById(Long id) {
+        Optional<Major> major = majorRepository.findById(id);
+        return major.map(m -> new MajorDTOResponse(m.getId(), m.getMajorName()));
     }
 
     @Override
     public List<MajorDTOResponse> getAll() {
-        return List.of();
+        List<Major> majors = majorRepository.findAll();
+        return majors.stream()
+                .map(m -> new MajorDTOResponse(m.getId(), m.getMajorName()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Boolean deleteById(Long id) {
-        return null;
+        if (majorRepository.existsById(id)) {
+            majorRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
+
