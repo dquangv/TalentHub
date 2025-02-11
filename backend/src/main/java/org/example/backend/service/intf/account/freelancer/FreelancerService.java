d95398788f9b7a1aaf26f6d02b0047bd41ab807d
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-11 13:56:20 +0700

    Update FreelancerService.java

--- backend/src/main/java/org/example/backend/service/intf/account/freelancer/FreelancerService.java
+++ backend/src/main/java/org/example/backend/service/intf/account/freelancer/FreelancerService.java
@@ -1,8 +1,8 @@
 package org.example.backend.service.intf.account.freelancer;
 
-import org.example.backend.dto.request.job.FreelancerJobDTORequest;
-import org.example.backend.dto.response.job.FreelancerJobDTOResponse;
+import org.example.backend.dto.request.account.freelancer.FreelancerDTORequest;
+import org.example.backend.dto.response.account.freelancer.FreelancerDTOResponse;
 import org.example.backend.service.BaseService;
 
-public interface FreelancerService extends BaseService<FreelancerJobDTORequest, FreelancerJobDTOResponse, Long> {
+public interface FreelancerService extends BaseService<FreelancerDTORequest, FreelancerDTOResponse, Long> {
 }
