8eefd93be9672e366120063870e8fb5b7ca2f9cb
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-11 13:56:12 +0700

    Update FreelancerDTOResponse.java

--- backend/src/main/java/org/example/backend/dto/response/account/freelancer/FreelancerDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/account/freelancer/FreelancerDTOResponse.java
@@ -1,4 +1,21 @@
 package org.example.backend.dto.response.account.freelancer;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class FreelancerDTOResponse {
+
+    private Long id;
+    private String image;
+    private BigDecimal hourlyRate;
+    private String description;
+    private String categoryName;
+    private Long userId;
 }
+
