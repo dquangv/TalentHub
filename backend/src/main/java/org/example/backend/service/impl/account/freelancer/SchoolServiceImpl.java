04d9daa4026707df616bc09dce007addbf3bb599
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-11 13:29:28 +0700

    Update SchoolServiceImpl.java

--- backend/src/main/java/org/example/backend/service/impl/account/freelancer/SchoolServiceImpl.java
+++ backend/src/main/java/org/example/backend/service/impl/account/freelancer/SchoolServiceImpl.java
@@ -3,33 +3,51 @@ package org.example.backend.service.impl.account.freelancer;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.request.account.freelancer.SchoolDTORequest;
 import org.example.backend.dto.response.account.freelancer.SchoolDTOResponse;
+import org.example.backend.entity.child.account.freelancer.School;
+import org.example.backend.repository.SchoolRepository;
 import org.example.backend.service.intf.account.freelancer.SchoolService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SchoolServiceImpl implements SchoolService {
 
+    private final SchoolRepository schoolRepository;
+
     @Override
-    public SchoolDTOResponse create(SchoolDTORequest schoolDTORequst) {
-        return null;
+    public SchoolDTOResponse create(SchoolDTORequest schoolRequestDTO) {
+        School school = new School();
+        school.setSchoolName(schoolRequestDTO.getSchoolName());
+
+        school = schoolRepository.save(school);
+
+        return new SchoolDTOResponse(school.getId(), school.getSchoolName());
     }
 
     @Override
-    public Optional<SchoolDTOResponse> getById(Long aLong) {
-        return Optional.empty();
+    public Optional<SchoolDTOResponse> getById(Long id) {
+        Optional<School> school = schoolRepository.findById(id);
+        return school.map(s -> new SchoolDTOResponse(s.getId(), s.getSchoolName()));
     }
 
     @Override
     public List<SchoolDTOResponse> getAll() {
-        return List.of();
+        List<School> schools = schoolRepository.findAll();
+        return schools.stream()
+                .map(s -> new SchoolDTOResponse(s.getId(), s.getSchoolName()))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Boolean deleteById(Long aLong) {
-        return null;
+    public Boolean deleteById(Long id) {
+        if (schoolRepository.existsById(id)) {
+            schoolRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
