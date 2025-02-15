2531799e7d8cb0acc1a2d2aa75e7fd1acad506f8
dquangvu <dquangvu7998@gmail.com>
2025-02-15 13:44:57 +0700

    add FreelancerJobDTO

--- backend/src/main/java/org/example/backend/dto/request/job/JobDTORequest.java
+++ backend/src/main/java/org/example/backend/dto/request/job/JobDTORequest.java
@@ -1,4 +1,14 @@
 package org.example.backend.dto.request.job;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobDTORequest {
+
 }
--- backend/src/main/java/org/example/backend/dto/response/job/JobDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/job/JobDTOResponse.java
@@ -1,4 +1,15 @@
 package org.example.backend.dto.response.job;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.enums.StatusFreelancerJob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobDTOResponse {
+
 }
