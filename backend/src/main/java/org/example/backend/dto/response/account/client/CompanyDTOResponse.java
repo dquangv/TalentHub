da9f11ab33ae05be6e452d980a76ee1c2048ad56
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-18 09:23:54 +0700

    Update CompanyDTOResponse.java

--- backend/src/main/java/org/example/backend/dto/response/account/client/CompanyDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/account/client/CompanyDTOResponse.java
@@ -1,4 +1,25 @@
 package org.example.backend.dto.response.account.client;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyDTOResponse {
+
+    private Long id;
+
+    private String companyName;
+
+    private String address;
+
+    private String phoneContact;
+
+    private String industry;
+
+    private Long clientId;
 }
