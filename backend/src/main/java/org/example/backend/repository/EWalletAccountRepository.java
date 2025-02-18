68ac51dab4348982ff3d447f93455a5f39e19991
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-18 09:25:41 +0700

    Update EWalletAccountRepository.java

--- backend/src/main/java/org/example/backend/repository/EWalletAccountRepository.java
+++ backend/src/main/java/org/example/backend/repository/EWalletAccountRepository.java
@@ -3,5 +3,9 @@ package org.example.backend.repository;
 import org.example.backend.entity.child.payment.EWalletAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EWalletAccountRepository extends JpaRepository<EWalletAccount, Long> {
+    Optional<EWalletAccount> findByEmail(String email);
+    Optional<EWalletAccount> findByPhoneNumber(String phoneNumber);
 }
