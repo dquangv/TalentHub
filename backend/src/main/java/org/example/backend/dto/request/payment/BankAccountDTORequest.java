c9f73adc13b1d6b845b50f9e0e7512ffed714f31
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-18 09:26:09 +0700

    Update BankAccountDTORequest.java

--- backend/src/main/java/org/example/backend/dto/request/payment/BankAccountDTORequest.java
+++ backend/src/main/java/org/example/backend/dto/request/payment/BankAccountDTORequest.java
@@ -14,26 +14,20 @@ import javax.validation.constraints.Size;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class BankAccountDTORequest {
     @NotBlank(message = "Bank name cannot be blank")
-    @Size(max = 100, message = "Bank name cannot exceed 100 characters")
     private String bankName;
 
-    @NotNull(message = "Bank account number cannot be null")
+    @NotBlank(message = "Bank account number cannot be null")
     private Integer bankAccountNumber;
 
     @NotBlank(message = "Branch cannot be blank")
-    @Size(max = 50, message = "Branch cannot exceed 50 characters")
     private String branch;
 
-    @NotNull(message = "Start date cannot be null")
-    @PastOrPresent(message = "Start date cannot be in the future")
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private DateTime startDate;
+    @NotBlank(message = "Start date is required")
+    private String startDate;
 
-    @NotNull(message = "Status cannot be null")
+    @NotBlank(message = "Status cannot be null")
     private Boolean status;
 
     @NotNull(message = "Payment id cannot be null")
