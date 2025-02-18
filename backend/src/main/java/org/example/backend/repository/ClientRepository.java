0e4601148ba12ae83bd5db9fdf760cdba7663b38
Đinh Huy <huy.dinh@pninformatics.com>
2025-02-18 09:25:16 +0700

    Update ClientRepository.java

--- backend/src/main/java/org/example/backend/repository/ClientRepository.java
+++ backend/src/main/java/org/example/backend/repository/ClientRepository.java
@@ -3,5 +3,5 @@ package org.example.backend.repository;
 import org.example.backend.entity.child.account.client.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
