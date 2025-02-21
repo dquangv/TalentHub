b939bdedbcee66c82618c96446882a879a1c98b0
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-02-21 15:27:09 +0700

    Added GroupAPis for Document

--- backend/src/main/java/org/example/backend/config/swagger/GroupApiDocs.java
+++ backend/src/main/java/org/example/backend/config/swagger/GroupApiDocs.java
@@ -14,6 +14,7 @@ public class GroupApiDocs {
                 .pathsToMatch("/api/v1/auth/**")
                 .build();
     }
+
     @Bean
     public GroupedOpenApi apiAccount() {
         return GroupedOpenApi.builder()
@@ -37,6 +38,7 @@ public class GroupApiDocs {
                 .pathsToMatch("/api/v1/jobs/**")
                 .build();
     }
+
     @Bean
     public GroupedOpenApi apiFreelancerSkill() {
         return GroupedOpenApi.builder()
@@ -44,6 +46,7 @@ public class GroupApiDocs {
                 .pathsToMatch("/api/v1/freelancer-skills/**")
                 .build();
     }
+
     @Bean
     public GroupedOpenApi apiSkills() {
         return GroupedOpenApi.builder()
@@ -51,6 +54,7 @@ public class GroupApiDocs {
                 .pathsToMatch("/api/v1/jobs/skills/**")
                 .build();
     }
+
     @Bean
     public GroupedOpenApi apiCategory() {
         return GroupedOpenApi.builder()
@@ -58,4 +62,85 @@ public class GroupApiDocs {
                 .pathsToMatch("/api/v1/categories/**")
                 .build();
     }
+
+    @Bean
+    public GroupedOpenApi apiClient() {
+        return GroupedOpenApi.builder()
+                .group("Client APIs")
+                .pathsToMatch("/api/v1/clients/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiCompany() {
+        return GroupedOpenApi.builder()
+                .group("Company APIs")
+                .pathsToMatch("/api/v1/companies/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiDegree() {
+        return GroupedOpenApi.builder()
+                .group("Degree APIs")
+                .pathsToMatch("/api/v1/degrees/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiEducation() {
+        return GroupedOpenApi.builder()
+                .group("Education APIs")
+                .pathsToMatch("/api/v1/educations/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiMajor() {
+        return GroupedOpenApi.builder()
+                .group("Major APIs")
+                .pathsToMatch("/api/v1/majors/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiSchool() {
+        return GroupedOpenApi.builder()
+                .group("School APIs")
+                .pathsToMatch("/api/v1/schools/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiJobSkill() {
+        return GroupedOpenApi.builder()
+                .group("JobSkill APIs")
+                .pathsToMatch("/api/v1/jobSkills/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiBankAccount() {
+        return GroupedOpenApi.builder()
+                .group("BankAccount APIs")
+                .pathsToMatch("/api/v1/bank-accounts/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiEWallet() {
+        return GroupedOpenApi.builder()
+                .group("EWallet APIs")
+                .pathsToMatch("/api/v1/e-wallets/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiPayments() {
+        return GroupedOpenApi.builder()
+                .group("Payments APIs")
+                .pathsToMatch("/api/v1/payments/**")
+                .build();
+    }
+
 }
