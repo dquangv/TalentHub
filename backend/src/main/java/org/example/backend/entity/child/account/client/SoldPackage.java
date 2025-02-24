754cc3d15a50320d4092ed490b7d7a50bbe35f71
dquangvu <dquangvu7998@gmail.com>
2025-02-24 16:28:53 +0700

    DROP DATABASE! add voucher package entity, sold package entity

---
+++ backend/src/main/java/org/example/backend/entity/child/account/client/SoldPackage.java
@@ -0,0 +1,40 @@
+package org.example.backend.entity.child.account.client;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.entity.AbstractEntity;
+import org.example.backend.entity.child.admin.VoucherPackage;
+
+import java.time.LocalDate;
+
+@Table(name = "sold_packages")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SoldPackage extends AbstractEntity<Long> {
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "status")
+    // true = valid; false = expired
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "voucher_packages")
+    private VoucherPackage voucherPackage;
+
+    @ManyToOne
+    @JoinColumn(name = "client")
+    private Client client;
+}
