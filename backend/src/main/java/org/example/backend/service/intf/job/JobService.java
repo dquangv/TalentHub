1ef21665f731863a21ebdbbfa4940af0f3222e49
thanhtung795 <vothanhtung795@gmail.com>
2025-02-24 19:52:22 +0700

    add api deital job

--- backend/src/main/java/org/example/backend/service/intf/job/JobService.java
+++ backend/src/main/java/org/example/backend/service/intf/job/JobService.java
@@ -1,11 +1,14 @@
 package org.example.backend.service.intf.job;
 
 import org.example.backend.dto.request.job.JobDTORequest;
+import org.example.backend.dto.response.job.DetailJobDTOResponse;
 import org.example.backend.dto.response.job.JobDTOResponse;
 import org.example.backend.service.BaseService;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobService extends BaseService<JobDTORequest, JobDTOResponse, Long> {
     List<JobDTOResponse> findAllJobs();
+    Optional<DetailJobDTOResponse> getDetailJobById(Long id);
 }
