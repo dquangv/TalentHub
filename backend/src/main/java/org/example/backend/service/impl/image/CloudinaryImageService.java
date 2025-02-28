f254b55b23dd25d04b9a821d0af511f8f6f46783
tiendinh203 <tientc0909@gmail.com>
2025-02-28 16:34:44 +0700

    inject value env for cloudinary

---
+++ backend/src/main/java/org/example/backend/service/impl/image/CloudinaryImageService.java
@@ -0,0 +1,29 @@
+package org.example.backend.service.impl.image;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CloudinaryImageService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryImageService(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
+    }
+
+}
