923be97a65ab881e3bc8a632e4856b4b9e3d3a78
tiendinh203 <tientc0909@gmail.com>
2025-02-28 16:46:45 +0700

    add /upload test service image upload

---
+++ backend/src/main/java/org/example/backend/controller/image/ImageController.java
@@ -0,0 +1,25 @@
+package org.example.backend.controller.image;
+import org.example.backend.service.impl.image.CloudinaryImageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    @Autowired
+    private CloudinaryImageServiceImpl cloudinaryImageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        String url = cloudinaryImageService.uploadImage(file);
+        return ResponseEntity.ok(url);
+    }
+
+
+
+}
