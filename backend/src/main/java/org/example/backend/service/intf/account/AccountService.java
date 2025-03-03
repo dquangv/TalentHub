e5b61a3b408bf264cd64b767945d45dfc2b17425
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-03 16:07:50 +0700

    Update AccountService.java

--- backend/src/main/java/org/example/backend/service/intf/account/AccountService.java
+++ backend/src/main/java/org/example/backend/service/intf/account/AccountService.java
@@ -7,7 +7,7 @@ import org.example.backend.service.BaseService;
 import java.util.List;
 
 public interface AccountService extends BaseService<AccountDTORequest, AccountDTOResponse, Long> {
-
+    Boolean checkEmail (String email);
     public List<AccountDTOResponse> getNearbyUsers(double lat, double lon, double distanceInMeters);
 }
 
