de56405f3e938d34e41aa71c5bc95a59abfea179
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-07 12:27:07 +0700

    Create ReportedJobMapper.java

---
+++ backend/src/main/java/org/example/backend/mapper/ReportedJobMapper.java
@@ -0,0 +1,26 @@
+package org.example.backend.mapper;
+
+import org.example.backend.dto.request.ReportedJobDTORequest;
+import org.example.backend.dto.response.ReportedJobDTOResponse;
+import org.example.backend.entity.child.ReportedJob;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface ReportedJobMapper {
+
+    @Mappings({
+            @Mapping(source = "freelancer.id", target = "freelancerId"),
+            @Mapping(source = "job.id", target = "jobId"),
+            @Mapping(source = "createdAt", target = "createdAt"),
+            @Mapping(source = "updatedAt", target = "updatedAt")
+    })
+    ReportedJobDTOResponse toDTO(ReportedJob reportedJob);
+
+    @Mappings({
+            @Mapping(source = "freelancerId", target = "freelancer.id"),
+            @Mapping(source = "jobId", target = "job.id")
+    })
+    ReportedJob toEntity(ReportedJobDTORequest reportedJobDTORequest);
+}
