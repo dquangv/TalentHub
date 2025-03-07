633cbd3faf055864fbc2e5b4ccf114d62260fa5e
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-07 12:27:16 +0700

    Create ReportedJobService.java

---
+++ backend/src/main/java/org/example/backend/service/intf/ReportedJobService.java
@@ -0,0 +1,12 @@
+package org.example.backend.service.intf;
+
+import org.example.backend.dto.request.ReportedJobDTORequest;
+import org.example.backend.dto.response.ReportedJobDTOResponse;
+import org.example.backend.service.BaseService;
+
+import java.util.List;
+
+public interface ReportedJobService  extends BaseService<ReportedJobDTORequest, ReportedJobDTOResponse, Long> {
+    ReportedJobDTOResponse update(Long id, ReportedJobDTORequest request);
+
+}
