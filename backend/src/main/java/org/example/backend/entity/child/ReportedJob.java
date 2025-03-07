37c2210b445ec7b4f98ca56e1fa3a3a7ec64713a
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-07 12:27:05 +0700

    Create ReportedJob.java

---
+++ backend/src/main/java/org/example/backend/entity/child/ReportedJob.java
@@ -0,0 +1,46 @@
+package org.example.backend.entity.child;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.entity.AbstractEntity;
+import org.example.backend.entity.child.account.freelancer.Freelancer;
+import org.example.backend.entity.child.job.Job;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportedJob extends AbstractEntity<Long> {
+    @Column(name = "title", nullable = false)
+    private String reasonFreelancer;
+    @Column(name = "image", nullable = false)
+    private String reasonAdmin;
+    @Column(name = "status", nullable = false)
+    private String status;
+
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at", insertable = false)
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    @ManyToOne
+    private Freelancer freelancer;
+
+    @ManyToOne
+    private Job job;
+}
