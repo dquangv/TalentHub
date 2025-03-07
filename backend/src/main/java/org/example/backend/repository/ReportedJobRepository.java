d83192368d9b3a5256084d9788ef95851ca530a2
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-07 12:28:58 +0700

    Update ReportedJobRepository.java

--- backend/src/main/java/org/example/backend/repository/ReportedJobRepository.java
+++ backend/src/main/java/org/example/backend/repository/ReportedJobRepository.java
@@ -3,5 +3,8 @@ package org.example.backend.repository;
 import org.example.backend.entity.child.ReportedJob;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportedJobRepository extends JpaRepository<ReportedJob, Long> {
+    List<ReportedJob> findByJobId(Long jobId);
 }
