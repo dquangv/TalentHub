d943690ce79b20cd5ef802bb99fa833a9c17fa2a
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-10 13:46:57 +0700

    Update ReportedJobDTOResponse.java

--- backend/src/main/java/org/example/backend/dto/response/ReportedJobDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/ReportedJobDTOResponse.java
@@ -2,18 +2,26 @@ package org.example.backend.dto.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.backend.dto.response.job.JobDTOResponse;
+import org.example.backend.entity.child.job.Job;
 import org.example.backend.enums.ReportedJobStatus;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ReportedJobDTOResponse {
     private Long id;
+    private String jobTitle;
     private String reasonFreelancer;
     private String reasonAdmin;
     private String description;
     private ReportedJobStatus status;
-    private String createdAt;
-    private String updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     private Long freelancerId;
     private Long jobId;
+    private String fullName;
+    private JobDTOResponse job;
+    private String image;
 }
