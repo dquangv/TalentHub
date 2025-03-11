44877b7bc160cc40800e240c88da44b12b59c3fc
tiendinh203 <tientc0909@gmail.com>
2025-03-11 14:36:20 +0700

    Create CV.java

---
+++ backend/src/main/java/org/example/backend/entity/child/account/freelancer/CV.java
@@ -0,0 +1,32 @@
+package org.example.backend.entity.child.account.freelancer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.entity.AbstractEntity;
+
+@Entity
+@Table(name = "cv")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CV extends AbstractEntity<Long> {
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "url", length = 500)
+    private String url;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private Freelancer freelancer;
+}
