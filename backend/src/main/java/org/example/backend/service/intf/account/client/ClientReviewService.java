aae35ae6d18580479f74c54cc1df34aef20e2fc9
dquangvu <dquangvu7998@gmail.com>
2025-03-13 15:39:50 +0700

    add freelancerReview api

--- backend/src/main/java/org/example/backend/service/intf/account/client/ClientReviewService.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.service.intf.account.client;
-
-import org.example.backend.dto.request.account.client.ClientReviewDTORequest;
-import org.example.backend.dto.response.account.client.ClientReviewDTOResponse;
-
-import org.example.backend.service.BaseService;
-
-public interface ClientReviewService extends BaseService<ClientReviewDTORequest, ClientReviewDTOResponse, Long> {
-}
--- backend/src/main/java/org/example/backend/service/intf/job/FreelancerJobService.java
+++ backend/src/main/java/org/example/backend/service/intf/job/FreelancerJobService.java
@@ -1,12 +1,15 @@
 package org.example.backend.service.intf.job;
 
 import org.example.backend.dto.request.account.freelancer.CreateFreelancerDTORequest;
+import org.example.backend.dto.request.job.ClientReviewDTORequest;
 import org.example.backend.dto.request.job.FreelancerJobDTORequest;
 import org.example.backend.dto.response.account.freelancer.ApplicantResponseDTO;
 import org.example.backend.dto.response.account.freelancer.CreateFreelancerDTOResponse;
+import org.example.backend.dto.response.job.ClientReviewDTOResponse;
 import org.example.backend.dto.response.job.FreelancerJobDTOResponse;
 import org.example.backend.dto.response.job.SaveJobDTOResponse;
 import org.example.backend.entity.child.account.client.Appointment;
+import org.example.backend.entity.child.account.client.ClientReview;
 import org.example.backend.entity.child.account.freelancer.CV;
 import org.example.backend.entity.child.job.FreelancerJob;
 import org.example.backend.service.BaseService;
@@ -25,4 +28,5 @@ public interface FreelancerJobService extends BaseService<FreelancerJobDTOReques
     FreelancerJob findById(Long jobId);
     public Appointment getAppointmentByFreelancerJobId(Long freelancerJobId);
     CV getCVByFreeLancer_IdAndJob_Id(Long freeLancerId, Long jobId);
+    ClientReviewDTOResponse freelancerReview(Long freelancerJobId, ClientReviewDTORequest request);
 }
