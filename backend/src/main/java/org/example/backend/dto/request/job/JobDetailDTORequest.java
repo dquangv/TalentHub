4f51f4f45ea48f34505d4162eff377cdd5e96f8e
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-13 10:57:21 +0700

    Create JobDetailDTORequest.java

---
+++ backend/src/main/java/org/example/backend/dto/request/job/JobDetailDTORequest.java
@@ -0,0 +1,33 @@
+package org.example.backend.dto.request.job;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.enums.StatusJob;
+import org.example.backend.enums.TypePayment;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobDetailDTORequest {
+    private String title;
+    private String description;
+    private String scope;
+    private BigDecimal hourWork;
+    private Long duration;
+    private Boolean jobOpportunity;
+    private BigDecimal fromPrice;
+    private BigDecimal toPrice;
+    private String typePrice;
+    private TypePayment typePayment;
+    private StatusJob statusJob;
+    private Long clientId;
+    private Long categoryId;
+    private List<Long> skillId;
+}
