4e359b3705b946f058cbc59cac307e0d8388b5a9
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-03-17 20:48:00 +0700

    ok

--- backend/src/main/java/org/example/backend/dto/request/job/JobAdminDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/request/job/JobAdminDTOResponse.java
@@ -17,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JobAdminDTOResponse {
+    private Long id;
     private String title;
     private StatusJob status;
     private String clientEmail;
