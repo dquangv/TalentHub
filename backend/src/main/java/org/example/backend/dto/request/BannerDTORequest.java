d0e7bff6d86bfb8983ecdf9e9213d6ec22734dbe
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-18 09:38:25 +0700

    Update BannerDTORequest.java

--- backend/src/main/java/org/example/backend/dto/request/BannerDTORequest.java
+++ backend/src/main/java/org/example/backend/dto/request/BannerDTORequest.java
@@ -1,9 +1,12 @@
 package org.example.backend.dto.request;
 
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -12,4 +15,6 @@ public class BannerDTORequest {
     private MultipartFile image;
     private String status;
     private String vendor;
+    private LocalDate startTime;
+    private LocalDate endTime;
 }
