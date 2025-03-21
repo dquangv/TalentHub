3426af18388adc0db0455ed427433d84868c4672
Đinh Huy <huy.dinh@pninformatics.com>
2025-03-21 12:46:35 +0700

    Update FreelancerRepository.java

--- backend/src/main/java/org/example/backend/repository/FreelancerRepository.java
+++ backend/src/main/java/org/example/backend/repository/FreelancerRepository.java
@@ -1,6 +1,8 @@
 package org.example.backend.repository;
 
 import org.example.backend.entity.child.account.freelancer.Freelancer;
+import org.example.backend.entity.child.account.freelancer.School;
+import org.example.backend.entity.child.job.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,10 +10,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 public interface FreelancerRepository extends JpaRepository<Freelancer,Long> {
     public Optional<Freelancer> findByUserId(Long userId);
     public Long countByCategoryId(Long categoryId);
-
+    List<Freelancer> findByCategory(Category category);
 }
