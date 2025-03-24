b85e35fb6f55a033fea1bafd0f59c3c748fac21f
dquangvu <dquangvu7998@gmail.com>
2025-03-24 12:50:50 +0700

    edit data type of status property of BannerDTOReSponse

--- backend/src/main/java/org/example/backend/dto/response/BannerDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/BannerDTOResponse.java
@@ -11,7 +11,7 @@ public class BannerDTOResponse {
     private Long id;
     private String title;
     private String image;
-    private String status;
+    private boolean status;
     private String vendor;
 //    private String createdAt;
 //    private String updatedAt;
