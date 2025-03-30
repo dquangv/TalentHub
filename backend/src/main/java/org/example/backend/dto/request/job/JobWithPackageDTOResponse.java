2cd9b4205df28a8f88abbd3ffffa6bf0821d99b6
tiendinh203 <tientc0909@gmail.com>
2025-03-30 18:16:59 +0700

    Create JobWithPackageDTOResponse.java

---
+++ backend/src/main/java/org/example/backend/dto/request/job/JobWithPackageDTOResponse.java
@@ -0,0 +1,28 @@
+package org.example.backend.dto.response.job;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backend.enums.TypePackage;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobWithPackageDTOResponse {
+    private Long id;
+    private String title;
+    private String companyName;
+    private String categoryName;
+    private BigDecimal hourWork;
+    private BigDecimal fromPrice;
+    private BigDecimal toPrice;
+    private String description;
+    private List<String> skillName;
+    private boolean seen;
+    private TypePackage typePackage;
+}
