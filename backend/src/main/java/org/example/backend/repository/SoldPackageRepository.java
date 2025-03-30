a5f4627f50f1910694260f98f45caaca0b45bbc3
tiendinh203 <tientc0909@gmail.com>
2025-03-30 17:50:52 +0700

    Update SoldPackageRepository.java

--- backend/src/main/java/org/example/backend/repository/SoldPackageRepository.java
+++ backend/src/main/java/org/example/backend/repository/SoldPackageRepository.java
@@ -1,6 +1,7 @@
 package org.example.backend.repository;
 
 import org.example.backend.entity.child.account.client.SoldPackage;
+import org.example.backend.enums.TypePackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,10 @@ public interface SoldPackageRepository extends JpaRepository<SoldPackage, Long> 
 
     SoldPackage findTopByClientIdAndStatusOrderByStartDateDesc(Long clientId, boolean status);
 
+
+    @Query("SELECT sp FROM SoldPackage sp " +
+            "WHERE sp.voucherPackage.typePackage = :type " +
+            "AND sp.status = true " + // Chỉ lấy package còn hiệu lực
+            "ORDER BY sp.startDate DESC")
+    List<SoldPackage> findByVoucherPackageType(TypePackage type);
 }
