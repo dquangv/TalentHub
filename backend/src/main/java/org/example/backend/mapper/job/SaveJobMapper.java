9b7a7ae0211d6b60c0f5f2546b5ba7dc85b65f0d
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-01 15:53:28 +0700

    Add field remainingTImeFormatted & remainingTimeInHours for SaveJobs

--- backend/src/main/java/org/example/backend/mapper/job/SaveJobMapper.java
+++ backend/src/main/java/org/example/backend/mapper/job/SaveJobMapper.java
@@ -3,16 +3,15 @@ package org.example.backend.mapper.job;
 import org.example.backend.dto.request.job.SaveJobDTORequest;
 import org.example.backend.dto.response.job.SaveJobDTOResponse;
 import org.example.backend.entity.child.job.FreelancerJob;
-import org.example.backend.entity.child.job.Job;
 import org.example.backend.mapper.BaseMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+import org.example.backend.utils.TimeRemainingUtils;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {TimeRemainingUtils.class})
 public interface SaveJobMapper extends BaseMapper<FreelancerJob, SaveJobDTORequest, SaveJobDTOResponse> {
 
     @Mapping(source = "job.id", target = "jobId")
@@ -23,6 +22,9 @@ public interface SaveJobMapper extends BaseMapper<FreelancerJob, SaveJobDTOReque
     @Mapping(source = "job.toPrice", target = "toPrice")
     @Mapping(source = "job.description", target = "description")
     @Mapping(expression = "java(mapSkillNames(freelancerJob))", target = "skillNames")
+    @Mapping(source = "job.endDate", target = "endDate")
+    @Mapping(target = "remainingTimeInHours", expression = "java(TimeRemainingUtils.calculateRemainingTimeInHours(freelancerJob.getJob().getEndDate()))")
+    @Mapping(target = "remainingTimeFormatted", expression = "java(TimeRemainingUtils.getFormattedTimeRemaining(freelancerJob.getJob().getEndDate()))")
     SaveJobDTOResponse toResponseDto(FreelancerJob freelancerJob);
 
     default List<String> mapSkillNames(FreelancerJob freelancerJob) {
