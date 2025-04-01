465670fe0943658a6b45effe38a51c8ae1964620
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-01 20:48:39 +0700

    Added createAt time format for JobWithPackage

--- backend/src/main/java/org/example/backend/dto/response/job/JobWithPackageDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/job/JobWithPackageDTOResponse.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.example.backend.enums.TypePackage;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -25,4 +26,6 @@ public class JobWithPackageDTOResponse {
     private List<String> skillName;
     private boolean seen;
     private TypePackage typePackage;
+    private Date createdAt;
+    private String createdTimeFormatted;
 }
