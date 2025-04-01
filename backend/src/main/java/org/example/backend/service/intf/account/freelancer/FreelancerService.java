6b4bd56a60b83871f9ebfde2515229b31a8f2612
dinhquochuyy <huy.dinh@pninformatics.com>
2025-04-01 17:10:58 +0700

    Update FreelancerService.java

--- backend/src/main/java/org/example/backend/service/intf/account/freelancer/FreelancerService.java
+++ backend/src/main/java/org/example/backend/service/intf/account/freelancer/FreelancerService.java
@@ -4,13 +4,17 @@ import org.example.backend.dto.request.account.freelancer.CreateFreelancerDTOReq
 import org.example.backend.dto.request.account.freelancer.FreelancerDTORequest;
 import org.example.backend.dto.request.account.freelancer.UpdateHourlyRateDTORequest;
 import org.example.backend.dto.response.account.freelancer.CreateFreelancerDTOResponse;
+import org.example.backend.dto.response.account.freelancer.FreelancerAdminDTOResponse;
 import org.example.backend.dto.response.account.freelancer.FreelancerDTOResponse;
 import org.example.backend.dto.response.account.freelancer.UpdateHourlyRateDTOResponse;
 import org.example.backend.service.BaseService;
+
+import java.util.List;
 
 public interface FreelancerService extends BaseService<FreelancerDTORequest, FreelancerDTOResponse, Long> {
     CreateFreelancerDTOResponse createProfile(CreateFreelancerDTORequest request);
     UpdateHourlyRateDTOResponse updateHourlyRate(UpdateHourlyRateDTORequest request);
     FreelancerDTOResponse updateCategory(Long freelancerId, Long categoryId);
+    public List<FreelancerAdminDTOResponse> getAllByAdmin();
 }
 
