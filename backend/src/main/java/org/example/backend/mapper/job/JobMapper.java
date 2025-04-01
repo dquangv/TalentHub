e66dc2d154e70ced5e6853a8901a261021c7913e
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-01 20:48:30 +0700

    Added createAt time format for JobDTO

--- backend/src/main/java/org/example/backend/mapper/job/JobMapper.java
+++ backend/src/main/java/org/example/backend/mapper/job/JobMapper.java
@@ -12,19 +12,22 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
-
 @Mapper(componentModel = "spring", imports = {TimeRemainingUtils.class})
 public interface JobMapper extends BaseMapper<Job, JobDTORequest, JobDTOResponse> {
 
     @Mapping(target = "skillName", expression = "java(mapSkills(job.getJobSkills()))")
     @Mapping(target = "categoryName", source = "category.categoryTitle")
     @Mapping(source = "endDate", target = "endDate")
+    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(target = "createdTimeFormatted", expression = "java(TimeRemainingUtils.getRelativeTimeFormatted(job.getCreatedAt()))")
     @Mapping(target = "remainingTimeInHours", expression = "java(TimeRemainingUtils.calculateRemainingTimeInHours(job.getEndDate()))")
     @Mapping(target = "remainingTimeFormatted", expression = "java(TimeRemainingUtils.getFormattedTimeRemaining(job.getEndDate()))")
     JobDTOResponse toResponseDto(Job job);
 
     @Mapping(target = "skillName", expression = "java(mapSkills(job.getJobSkills()))")
     @Mapping(target = "categoryName", source = "job.category.categoryTitle")
+    @Mapping(source = "job.createdAt", target = "createdAt")
+    @Mapping(target = "createdTimeFormatted", expression = "java(TimeRemainingUtils.getRelativeTimeFormatted(job.getCreatedAt()))")
     @Mapping(target = "typePackage", source = "typePackage")
     JobWithPackageDTOResponse toResponseWithPackageDto(Job job, TypePackage typePackage);
 
