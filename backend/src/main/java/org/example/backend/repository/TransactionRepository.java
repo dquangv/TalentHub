6f0e5bf223790ade6689c37c69df173fc7f01c09
thanhtung795 <vothanhtung795@gmail.com>
2025-04-06 22:56:09 +0700

    added entity transactions  and fix accoutn and payment 1 -1 , fixx  3 api   in payment

---
+++ backend/src/main/java/org/example/backend/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package org.example.backend.repository;
+
+import org.example.backend.entity.child.payment.Transactions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transactions, Long> {
+}
