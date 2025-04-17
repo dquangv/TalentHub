7ab6fa376952760b5f687aca67bfe2d314fec384
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-17 13:29:26 +0700

    Added Find by clientid

--- backend/src/main/java/org/example/backend/repository/CompanyRepository.java
+++ backend/src/main/java/org/example/backend/repository/CompanyRepository.java
@@ -3,8 +3,10 @@ package org.example.backend.repository;
 import org.example.backend.entity.child.account.client.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Optional<Company> getCompanyByClientId(Long aLong);
+    List<Company> findByClientId(Long clientId);
 }
