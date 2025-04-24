f33e817d93d1edae69f20044f7ab8ccf2c3972d4
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-24 16:22:35 +0700

    Added more info for review

--- backend/src/main/java/org/example/backend/dto/response/job/ClientReviewDTOResponse.java
+++ backend/src/main/java/org/example/backend/dto/response/job/ClientReviewDTOResponse.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ClientReviewDTOResponse {
     private Long id;
     private Float rating;
     private String note;
     private String reviewerName;
+
+    private String projectTitle;
+    private LocalDateTime projectStartDate;
+    private Long projectDuration;
+    private BigDecimal projectBudget;
+
+    private String freelancerAvatar;
 }
