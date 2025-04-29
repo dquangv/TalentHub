bdc4ed7621e0fadb8867be78bc84803fd614dcba
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-29 14:09:44 +0700

    Added DTO request for verify, checking muti-factor-authen

---
+++ backend/src/main/java/org/example/backend/dto/response/account/MfaVerificationResponseDTO.java
@@ -0,0 +1,15 @@
+package org.example.backend.dto.response.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MfaVerificationResponseDTO {
+    private boolean verified;
+}
+
