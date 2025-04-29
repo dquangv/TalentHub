cd0a31b2ab285fd69aa1877c4a34d9128d5c2874
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-29 14:08:16 +0700

    Added Endpoint to set up, reset Multi-Factor Authentication

--- backend/src/main/java/org/example/backend/controller/account/AuthController.java
+++ backend/src/main/java/org/example/backend/controller/account/AuthController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.ResponseObject;
 import org.example.backend.dto.request.account.AuthenticationDTORequest;
 import org.example.backend.dto.request.account.IntrospectDTORequest;
+import org.example.backend.dto.request.account.MfaLoginVerificationRequest;
 import org.example.backend.dto.response.account.AuthenticationDtoResponse;
 import org.example.backend.dto.response.account.IntrospectDtoResponse;
+import org.example.backend.dto.response.account.MfaChallengeResponse;
 import org.example.backend.dto.response.account.RefreshTokenDTOResponse;
 import org.example.backend.service.intf.account.AuthenticationService;
 import org.springframework.web.bind.annotation.*;
@@ -22,8 +24,27 @@ public class AuthController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseObject<AuthenticationDtoResponse> authenticate(@Valid @RequestBody AuthenticationDTORequest request) throws JOSEException {
-        AuthenticationDtoResponse response = authenticationService.authenticate(request);
+    public ResponseObject<?> authenticate(@Valid @RequestBody AuthenticationDTORequest request) throws JOSEException {
+        Object response = authenticationService.authenticate(request);
+
+        if (response instanceof MfaChallengeResponse) {
+            // Yêu cầu xác thực 2FA
+            return ResponseObject.builder()
+                    .message("Requires 2FA verification")
+                    .status(200)
+                    .data(response)
+                    .build();
+        } else {
+            return ResponseObject.builder()
+                    .message("Login successful")
+                    .status(200)
+                    .data(response)
+                    .build();
+        }
+    }
+    @PostMapping("/verify-mfa")
+    public ResponseObject<AuthenticationDtoResponse> verifyMfaAndLogin(@Valid @RequestBody MfaLoginVerificationRequest request) throws JOSEException {
+        AuthenticationDtoResponse response = authenticationService.verifyMfaAndLogin(request);
 
         return ResponseObject.<AuthenticationDtoResponse>builder()
                 .message("Login successful")
@@ -31,7 +52,6 @@ public class AuthController {
                 .data(response)
                 .build();
     }
-
     @PostMapping("/refresh-token")
     public ResponseObject<RefreshTokenDTOResponse> refreshToken(@RequestParam String refreshToken) throws JOSEException, ParseException {
         RefreshTokenDTOResponse response = authenticationService.refreshToken(refreshToken);
