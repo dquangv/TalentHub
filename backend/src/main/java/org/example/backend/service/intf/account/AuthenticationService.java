94627e56ed02b9277277934eb5ef02eed5b0390e
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-29 14:11:15 +0700

    Feat feature for MFA - 2FA

--- backend/src/main/java/org/example/backend/service/intf/account/AuthenticationService.java
+++ backend/src/main/java/org/example/backend/service/intf/account/AuthenticationService.java
@@ -3,6 +3,7 @@ package org.example.backend.service.intf.account;
 import com.nimbusds.jose.JOSEException;
 import org.example.backend.dto.request.account.AuthenticationDTORequest;
 import org.example.backend.dto.request.account.IntrospectDTORequest;
+import org.example.backend.dto.request.account.MfaLoginVerificationRequest;
 import org.example.backend.dto.response.account.RefreshTokenDTOResponse;
 import org.example.backend.dto.response.account.AuthenticationDtoResponse;
 import org.example.backend.dto.response.account.IntrospectDtoResponse;
@@ -10,7 +11,8 @@ import org.example.backend.dto.response.account.IntrospectDtoResponse;
 import java.text.ParseException;
 
 public interface AuthenticationService {
-    AuthenticationDtoResponse authenticate(AuthenticationDTORequest request) throws JOSEException;
+    Object authenticate(AuthenticationDTORequest request) throws JOSEException;
+    AuthenticationDtoResponse verifyMfaAndLogin(MfaLoginVerificationRequest request) throws JOSEException;
 
     IntrospectDtoResponse introspect(IntrospectDTORequest request) throws JOSEException, ParseException;
 
